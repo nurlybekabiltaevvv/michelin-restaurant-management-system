44608a62e05d07e61902830f35a44334ea8c2281
Sanzhar <sa_yeraly@kbtu.kz>
2026-06-24 16:51:38 +0500

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/michelin-restaurant-management-system/', // например: '/my-app/'
+  base: '/michelin-restaurant-management-system/',
 })
